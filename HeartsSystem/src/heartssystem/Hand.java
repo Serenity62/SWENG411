2f952127bcd3510c3696ba00598dd87e481f5d4e
Serenity62 <cuz126@psu.edu>
2016-12-13 11:42:52 -0500

    Fix the play card to trick logic. Forces the 2 of clubs at the start of round, forces suit, and allows Hearts Broken. Need to check out next player logic between tricks.

--- HeartsSystem/src/heartssystem/Hand.java
+++ HeartsSystem/src/heartssystem/Hand.java
@@ -8,19 +8,9 @@ public class Hand extends CardSet {
     public boolean hasSuit(int suit) {
         boolean hasSuit = false;
         //sortHand();
-        int low = 0;
-        int hi = this.getSize() - 1;
-        int mid;
-        while (low <= hi) {
-            mid = low + (hi - low) / 2;
-            if (getCard(mid).getSuit() < suit) {
-                hi = mid - 1;
-            } else if (getCard(mid).getSuit() > suit) {
-                low = mid + 1;
-            } else {
+        for(int i = 0; i < this.getSize() && !hasSuit; i++){
+            if(this.getCard(i).getSuit() == suit)
                 hasSuit = true;
-                break;
-            }
         }
         return hasSuit;
     }
